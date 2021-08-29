c60a6472fa2969b7e9598329f3cfed7294c7b998
Boba Fett <thomascupchurch@gmail.com>
2021-08-29 13:43:45 -0500

    delete route working

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -11,34 +11,30 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
     // receive a new note to save on the request body, 
-    const newNote = {text: req.body, id: uuidv4()};
+    const db = JSON.parse(fs.readFileSync('./db/db.json'));
     
+    const newNote = {title: req.body.title, text: req.body.text, id: uuidv4()};
     
-    const stringNote = JSON.stringify(newNote);
-    fs.writeFileSync('./db/db.json', stringNote);
-    res.json(stringNote);
-    
-    // add it to the db.json file,
-    // fs.writeFileSync(notes, json, 'utf8', callback);
-    // fs.readFile(notes, 'utf8', function readFileCallback(err, data){
-    //     if(err){
-    //         console.log(err);
-    //     } else {
-    //         obj = JSON.parse(data);
-    //         obj.table.push({id: })
-    //     }
-    // })
-    
-    // then return the new note to the client
-    
+    db.push(newNote);
 
-    
+    const stringDb = JSON.stringify(db);
+
+    fs.writeFileSync('./db/db.json', stringDb);
+    res.json(db);
   });
 
 //   DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 router.delete('/notes/:id', (req, res) => {
+    const db = JSON.parse(fs.readFileSync('./db/db.json'));
+    
     const toDelete = req.params.id;
-    deleteNote(toDelete);
+     
+    const editedDb = db.filter(note => note.id !== toDelete);
+    
+    const stringEditedDb = JSON.stringify(editedDb);
+
+    fs.writeFileSync('./db/db.json', stringEditedDb);
+
     res.send("deleted");
 });
 
