69cb79d1281f63e03483ca4af2e1afded7976f4d
Boba Fett <thomascupchurch@gmail.com>
2021-08-29 12:16:47 -0500

    writing to json file

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,31 +1,45 @@
 const router = require('express').Router();
 const fs = require("fs");
-// const { notes } = require('../../Develop/db/db.json');
+const { notes } = require('../../db/db.json');
 const { v4: uuidv4 } = require('uuid');
 
+
 router.get('/notes', (req, res) => {
-    const db = JSON.parse(fs.readFileSync("./db/db.json"));
+    const db = /*JSON.parse*/fs.readFileSync('./db/db.json')/*)*/;
     res.send(db);
 });
 
 router.post('/notes', (req, res) => {
     // receive a new note to save on the request body, 
     const newNote = {text: req.body, id: uuidv4()};
+    
+    
+    const stringNote = JSON.stringify(newNote);
+    fs.writeFileSync('./db/db.json', stringNote);
     res.json(newNote);
+    
     // add it to the db.json file,
-    const db = JSON.parse(fs.writeFileSync("./db/noteFile.json"))
-    res.send(db);
+    // fs.writeFileSync(notes, json, 'utf8', callback);
+    // fs.readFile(notes, 'utf8', function readFileCallback(err, data){
+    //     if(err){
+    //         console.log(err);
+    //     } else {
+    //         obj = JSON.parse(data);
+    //         obj.table.push({id: })
+    //     }
+    // })
+    
     // then return the new note to the client
     
-    
-    // give each note a unique id when it's saved (research npm packages) npm install uniqid
+
     
   });
-  
 
 //   DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
-// router.delete('/notes/:id', (req, res) )
-// const db = JSON.parse()
-
+router.delete('/notes/:id', (req, res) => {
+    const toDelete = req.params.id;
+    deleteNote(toDelete);
+    res.send("deleted");
+});
 
 module.exports = router;
