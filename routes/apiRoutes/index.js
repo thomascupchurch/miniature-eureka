4a499c6a65cac5044ca3f75927d1f62d8329d3dd
Boba Fett <thomascupchurch@gmail.com>
2021-08-29 14:05:55 -0500

    file restructure for deploy

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const fs = require("fs");
 const { notes } = require('../../db/db.json');
-
-import {v4 as uuidv4} from 'uuid';
 const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
